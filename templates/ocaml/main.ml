4f9d054259b47f64dd83bb41d2ec8d4f6a9e4d59
joseph-amirth <epicjoe128@gmail.com>
2025-01-17 21:29:30 +0530

    Update ocaml template

--- templates/ocaml/main.ml
+++ templates/ocaml/main.ml
@@ -1,14 +1,13 @@
 let read_file file_name =
-  let rec read_file_impl file_handle =
+  let file_handle = open_in file_name in
+  let rec read_file_impl () =
     let open Option in
     let maybe_line =
       try Some (input_line file_handle) with End_of_file -> None
     in
-    match maybe_line with
-    | Some line -> line :: read_file_impl file_handle
-    | None -> []
+    match maybe_line with Some line -> line :: read_file_impl () | None -> []
   in
-  read_file_impl (open_in file_name)
+  read_file_impl ()
 
 let input_lines = read_file "input.txt"
-let _ = List.iter print_endline input_lines
+let () = List.iter print_endline input_lines
