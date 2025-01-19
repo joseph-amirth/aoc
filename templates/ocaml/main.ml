ebe2209bdd1032db989cca867bf8c917680c806c
joseph-amirth <epicjoe128@gmail.com>
2025-01-19 11:18:14 +0530

    Update ocaml template to use Base and Stdio

--- templates/ocaml/main.ml
+++ templates/ocaml/main.ml
@@ -1,12 +1,9 @@
-let read_file file_name =
-  let file_handle = open_in file_name in
-  let rec read_file_impl () =
-    let maybe_line =
-      try Some (input_line file_handle) with End_of_file -> None
-    in
-    match maybe_line with Some line -> line :: read_file_impl () | None -> []
-  in
-  read_file_impl ()
+open Base
+open Stdio
 
-let input_lines = read_file "input.txt"
-let () = List.iter print_endline input_lines
+let input_lines filename =
+  let file = In_channel.create filename in
+  In_channel.input_lines file
+
+let input_lines = input_lines "input.txt"
+let () = List.iter ~f:print_endline input_lines
