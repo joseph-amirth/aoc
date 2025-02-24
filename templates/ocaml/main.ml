c86b929aace184b73c79937661576f59d68d6183
joseph-amirth <epicjoe128@gmail.com>
2025-02-24 20:22:34 +0530

    Make ocaml template more concise

--- templates/ocaml/main.ml
+++ templates/ocaml/main.ml
@@ -1,5 +1,6 @@
 open Base
 open Stdio
 
-let input_lines = In_channel.create "input.txt" |> In_channel.input_lines
-let () = List.iter ~f:print_endline input_lines
+let () =
+  In_channel.create "input.txt"
+  |> In_channel.input_lines |> List.iter ~f:print_endline
